d5bfed429ab0beaa078de2ac0c3148d2b6bc51ed
nonplus <perrisdavis@me.com>
2021-03-03 19:18:25 -0800

    Initial Commit

---
+++ Different/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Different
+//
+//  Created by nonplus on 3/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
